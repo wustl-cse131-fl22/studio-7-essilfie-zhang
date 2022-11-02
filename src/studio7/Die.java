4852dd68cc042b9dc0664527aa2fa7d39a764db7
giselleessilfie <giselleessilfie@localhost>
2022-11-02 17:18:00 -0500

    hi

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -1,5 +1,14 @@
 package studio7;
 
 public class Die {
+	private int integer;
+	
+	public int die(int n) {
+	
+		integer = (int)Math.random()*n+1; 
+		
+		return integer;
+	}
+	
 
 }
--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -1,5 +1,17 @@
 package studio7;
 
 public class Fraction {
-
+	private double numerator;
+	private double denominator; 
+	public double sumofFraction(Fraction B) {
+		double denominator;
+		return productofFraction(null);
+		
+	}
+	public double productofFraction(Fraction A) {
+		 double newNum =this.numerator * A.numerator;
+		 double newDen = this.denominator * A.denominator;
+		 return newNum/newDen;
+	
+	}
 }
